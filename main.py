2f0b5e1652e9b1f37b5f8d1c76f7bb66add92d3c
Adilson Torres <adilson.torres@outlook.com>
2018-09-16 03:01:48 -0300

    Refactor, add port, debug_port options

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import time
 import numpy as np
 import requests
 
+MAX_SIZE = 65536 - 8  # less 8 bytes of video time
+
 
 def arg_parse():
     parser = argparse.ArgumentParser(description='Server')
@@ -14,8 +16,10 @@ def arg_parse():
     parser.add_argument("--fps", help="Set video FPS", type=int, default=14)
     parser.add_argument("--gray", help="Convert video into gray scale", action="store_true")
     parser.add_argument("--ip", help="Server IP address", default="localhost")
+    parser.add_argument("--port", help="UDP port number", type=int, default=60444)
     parser.add_argument("--rasp_simulator", help="This is not a Raspberry Pi", action="store_true")
     parser.add_argument("--debug_ip", help="IP where the server will retransmit the video", default=None)
+    parser.add_argument("--debug_port", help="Port where the server will retransmit the video", default=None)
 
     return parser.parse_args()
 
@@ -29,33 +33,11 @@ def connected_to_internet(url='http://www.google.com/', timeout=5):
     return False
 
 
-def main(args):
-    register_url = 'http://{}:8000/object-detection/register/'.format(args.ip)
-    cam_id = None
-
-    while not connected_to_internet():
-        pass
-
-    if args.rasp_simulator:
-        cam_id = -1
-    else:
-        # TODO: Get rasp id
-        pass
-
-    payload = {'cam_id': cam_id}
-    if args.debug_ip:
-        payload['debug_ip'] = args.debug_ip
-
-    register = requests.post(url=register_url, data=payload)
-    port = int(register.text)
-
-    address = (args.ip, port)
-
-    cap = cv2.VideoCapture(args.video)
+def send_video(address, video, desired_fps, gray):
+    cap = cv2.VideoCapture(video)
 
     video_fps = cap.get(cv2.CAP_PROP_FPS)
-    desired_fps = args.fps
-    max_size = 65536 - 8  # less 8 bytes of video time
+
     if desired_fps > video_fps:
         desired_fps = video_fps
 
@@ -65,19 +47,20 @@ def main(args):
         transmission_start = time.time()
         processing_start = time.time()
         jpg_quality = 80
+
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
 
-            if args.gray:
+            if gray:
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpg_quality]
             result, encoded_img = cv2.imencode('.jpg', frame, encode_param)
 
             # Decrease quality until frame size is less than 65k
-            while encoded_img.nbytes > max_size:
+            while encoded_img.nbytes > MAX_SIZE:
                 jpg_quality -= 5
                 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpg_quality]
                 result, encoded_img = cv2.imencode('.jpg', frame, encode_param)
@@ -103,11 +86,38 @@ def main(args):
             processing_start = time.time()
 
     except KeyboardInterrupt:
-        cap.release()
-        sock.close()
+        pass
 
     cap.release()
     sock.close()
+
+
+def main(args):
+    register_url = 'http://{}:8000/object-detection/register/'.format(args.ip)
+    cam_id = None
+
+    while not connected_to_internet():
+        pass
+
+    if args.rasp_simulator:
+        cam_id = -1
+    else:
+        # TODO: Get rasp id, zerotier address.
+        cam_id = 1
+        # pass
+
+    payload = {'cam_id': cam_id, 'port': args.port}
+    if args.debug_ip:
+        payload['debug_ip'] = args.debug_ip
+    if args.debug_port:
+        payload['debug_port'] = args.debug_port
+
+    register = requests.post(url=register_url, data=payload)
+    port = int(register.text)
+
+    address = (args.ip, port)
+
+    send_video(address, args.video, args.fps, args.gray)
 
 
 if __name__ == '__main__':
