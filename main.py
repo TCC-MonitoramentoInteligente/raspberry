220f12087a323cd824b4a520938203741fb53f02
Adilson Torres <adilson.torres@outlook.com>
2018-10-26 11:14:39 -0300

    Refactor raspberry, remove mqtt connection

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import requests
 import paho.mqtt.client as mqtt
 
 
+# BROKER_IP = '10.1.0.4'
 BROKER_IP = 'localhost'
 MAX_SIZE = 65536 - 8  # less 8 bytes of video time
 
@@ -19,13 +20,14 @@ def arg_parse():
     parser.add_argument("--video", help="Path to video file", default=0)
     parser.add_argument("--fps", help="Set video FPS", type=int, default=14)
     parser.add_argument("--gray", help="Convert video into gray scale", action="store_true")
-    parser.add_argument("--ip", help="Server IP address", default="localhost")
-    parser.add_argument("--port", help="UDP port number", type=int, default=60444)
     parser.add_argument("--rasp_simulator", help="This is not a Raspberry Pi", action="store_true")
-    parser.add_argument("--debug_ip", help="IP where the server will retransmit the video", default=None)
-    parser.add_argument("--debug_port", help="Port where the server will retransmit the video", default=None)
 
     return parser.parse_args()
+
+
+def get_id():
+    # TODO: get zerotier id
+    return 1
 
 
 def connected_to_internet(url='http://www.google.com/', timeout=5):
@@ -96,56 +98,31 @@ def send_video(address, video, desired_fps, gray):
     sock.close()
 
 
-def init_broker():
-    mqtt_client = mqtt.Client()
-    try:
-        mqtt_client.connect(BROKER_IP)
-    except ConnectionRefusedError:
-        time.sleep(5)
-        return False
-    mqtt_client.loop_start()
-    return mqtt_client
-
-
 def main(args):
-    register_url = 'http://{}:8000/object-detection/register/'.format(args.ip)
-    cam_id = None
+    register_url = 'http://10.0.1.2:8080/object-detection/register/'
 
     while not connected_to_internet():
         time.sleep(5)
         pass
 
-    mqtt_client = None
-    while not mqtt_client:
-        mqtt_client = init_broker()
-
     if args.rasp_simulator:
-        cam_id = -1
-    else:
-        # TODO: Get rasp id, zerotier address.
         cam_id = 1
-        # pass
+    else:
+        cam_id = get_id()
 
-    payload = {'cam_id': cam_id, 'port': args.port}
-    if args.debug_ip:
-        payload['debug_ip'] = args.debug_ip
-    if args.debug_port:
-        payload['debug_port'] = args.debug_port
+    tries = 10
+    while tries > 0:
+        try:
+            register = requests.post(url=register_url, data={'cam_id': cam_id})
 
-    try:
-        register = requests.post(url=register_url, data=payload)
-        mqtt_client.publish(topic="raspberry/register", payload=cam_id)
-    except requests.ConnectionError:
-        print("Unable to connect with object detection service")
-        mqtt_client.publish(topic="raspberry/fail_register", payload=cam_id)
-        time.sleep(2)
-        sys.exit(0)
+            port = int(register.text)
+            address = (args.ip, port)
+            send_video(address, args.video, args.fps, args.gray)
+            tries = 0
 
-    port = int(register.text)
-
-    address = (args.ip, port)
-
-    send_video(address, args.video, args.fps, args.gray)
+        except requests.ConnectionError:
+            print("Unable to connect with object detection service")
+            tries -= 1
 
 
 if __name__ == '__main__':
